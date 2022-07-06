4bdeeae82b4483ba04229b398ba1ada783376e5f
benjamin huang <benjaminhuanghuang@gmail.com>
2022-07-06 12:47:23 -0700

    kernel debug

--- Kernel-debug/start-qemu.sh
+++ Kernel-debug/start-qemu.sh
@@ -1,5 +1,4 @@
 qemu-system-x86_64 -kernel ~/workspace/linux/arch/x86/boot/bzImage \
   -initrd ~/workspace/rootfs/rootfs.img \
-  -append "root=/dev/sda console=ttyS0" \
-  -nographic \
-  nokaslr -s -S
+  -append "root=/dev/sda console=ttyS0" nokaslr -s -S \
+  -nographic 
